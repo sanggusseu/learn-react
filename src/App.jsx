3e630e5522416bd2a45b55d03e3900ce2982d027
sanggusseu <sanggusseu@gmail.com>
2024-09-13 17:05:37 +0900

    실습 - 강의목록 컴포넌트

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CourseListCard from './components/course/CourseListCard';
+
+function App() {
+  return (
+    <>
+      <main>
+        <CourseListCard />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/AppCounter.jsx
@@ -0,0 +1,19 @@
+import './AppCounter.css'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App

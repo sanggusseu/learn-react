166599e3f37d4ffc37b86f9de4ae28aa0605de50
sanggusseu <sanggusseu@gmail.com>
2024-09-15 11:32:54 +0900

    중첩 객체 업데이트

--- src/components/course/CourseForm.jsx
+++ src/components/course/CourseForm.jsx
@@ -5,6 +5,10 @@ export default function CourseForm() {
   const [form, setForm] = useState({
     title: '리액트 강의',
     description: '리액트 기초부터 실전까지!',
+    info: {
+      level: 1,
+      skill: 'React',
+    },
   });
 
   const handleSubmit = e => {
@@ -13,6 +17,19 @@ export default function CourseForm() {
 
   const handleChange = e => {
     setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSkillChange = e => {
+    setForm({
+      ...form,
+      info: { ...form.info, [e.target.name]: e.target.value },
+    });
+  };
+  const handleLevelChange = e => {
+    setForm({
+      ...form,
+      info: { ...form.info, [e.target.name]: e.target.value },
+    });
   };
   return (
     <Card title='강의 등록'>
@@ -32,6 +49,33 @@ export default function CourseForm() {
           type='text'
           placeholder='강의 한줄 설명'
         />
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <label htmlFor='skill' style={{ width: '100px' }}>
+            스킬
+          </label>
+          <input
+            name='skill'
+            id='skill'
+            type='text'
+            value={form.info.skill}
+            onChange={handleSkillChange}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <label htmlFor='level' style={{ width: '100px' }}>
+            난이도
+          </label>
+          <select
+            name='level'
+            id='level'
+            value={form.info.level}
+            onChange={handleLevelChange}
+          >
+            <option value='0'>입문</option>
+            <option value='1'>초급</option>
+            <option value='2'>중급</option>
+          </select>
+        </div>
         <input type='submit' value='등록' />
       </form>
       {(form.title || form.description) && (
@@ -45,6 +89,8 @@ export default function CourseForm() {
         >
           {form.title && <p>제목 - {form.title}</p>}
           {form.description && <p>설명 - {form.description}</p>}
+          {form.info.skill && <p>설명 - {form.info.skill}</p>}
+          {form.info.level && <p>설명 - {form.info.level}</p>}
         </div>
       )}
     </Card>

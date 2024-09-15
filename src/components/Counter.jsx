a3e9c55119324ae7c6f1f25020a9a525dd5d1fbb
sanggusseu <sanggusseu@gmail.com>
2024-09-15 10:29:13 +0900

    batching & 업데이터 함수

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react';
 function Counter({ onTotal }) {
   const [counter, setCounter] = useState(0);
 
-  console.log('Counter');
+  console.log('[렌더링] Counter: ', counter);
 
   const handleCounter = () => {
     setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    console.log('[함수호출] Counter: ', counter);
+
     if (onTotal) {
-      onTotal();
+      onTotal;
     }
   };
 
   return (
     <>
       <button onClick={handleCounter}>Counter {counter} </button>
-      <input type='text' />
     </>
   );
 }

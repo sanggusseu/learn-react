883137a713668b14dc2a2c3693b418e9ceb8aa85
sanggusseu <sanggusseu@gmail.com>
2024-09-17 14:23:11 +0900

    Context 상태공유 2탄

--- src/AppTheme.jsx
+++ src/AppTheme.jsx
@@ -2,20 +2,15 @@ import './AppTheme.css';
 import HeaderTheme from './components/theme/Header.jsx';
 import MainTheme from './components/theme/Main.jsx';
 import FooterTheme from './components/theme/Footer.jsx';
-import { useState } from 'react';
-import { ModeContext } from './context/ModeContext.jsx';
+import { ModeProvider } from './context/ModeContext.jsx';
 function AppTheme(props) {
-  const [mode, setMode] = useState('light');
-  const handleMode = () => {
-    setMode(mode === 'light' ? 'dark' : 'light');
-  };
   return (
     <>
-      <ModeContext.Provider value={{ mode, handleMode }}>
+      <ModeProvider>
         <HeaderTheme />
         <MainTheme />
         <FooterTheme />
-      </ModeContext.Provider>
+      </ModeProvider>
     </>
   );
 }
--- src/context/ModeContext.jsx
+++ src/context/ModeContext.jsx
@@ -1,3 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const ModeContext = createContext('dark');
+
+export const ModeProvider = ({ children }) => {
+  const [mode, setMode] = useState('light');
+  const handleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ModeContext.Provider value={{ mode, handleMode }}>
+      {children}
+    </ModeContext.Provider>
+  );
+};

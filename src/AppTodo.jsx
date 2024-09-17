c60d39d0bb3e0c350c54d15bae69020516aab1bc
sanggusseu <sanggusseu@gmail.com>
2024-09-17 11:55:38 +0900

    State 로직을 리듀서로 작성하기

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import './App.css';
 import TodoList from './components/todo/TodoList';
+import todoReducer from './reducer/todoReducer';
 
 function AppTodo(props) {
   const [todoText, setTodoText] = useState('');
-  const [todos, setTodos] = useState([
+  const [todos, dispatch] = useReducer(todoReducer, [
     { id: 0, text: 'HTML&CSS 공부하기', done: false },
     { id: 1, text: '자바스크립트 공부하기', done: false },
   ]);
@@ -13,49 +14,79 @@ function AppTodo(props) {
   const handleTodoTextChange = e => {
     setTodoText(e.target.value);
   };
+  // 1] added
   const handleAddTodo = e => {
-    const nextId = todos.length;
-    setTodos([...todos, { id: nextId, text: todoText, done: false }]);
+    dispatch({
+      type: 'added',
+      nextId: todos.length,
+      todoText,
+    });
+    // const nextId = todos.length;
+    // setTodos([...todos, { id: nextId, text: todoText, done: false }]);
     setTodoText(''); // null, undefined [X]
   };
+
+  // 2] added_index
   const handleAddTodoByIndex = () => {
-    const nextId = todos.length;
-    const newTodos = [
-      // 삽입 지점 이전 항목
-      ...todos.slice(0, insertAt),
-      // 새 항목
-      { id: nextId, text: todoText, done: false },
-      // 삽입 지점 이후 항목
-      ...todos.slice(insertAt),
-    ];
-    setTodos(newTodos);
-    setName('');
+    dispatch({
+      type: 'added_index',
+      nextId: todos.length,
+      todoText,
+      insertAt,
+    });
+    // const nextId = todos.length;
+    // const newTodos = [
+    //   // 삽입 지점 이전 항목
+    //   ...todos.slice(0, insertAt),
+    //   // 새 항목
+    //   { id: nextId, text: todoText, done: false },
+    //   // 삽입 지점 이후 항목
+    //   ...todos.slice(insertAt),
+    // ];
+    // setTodos(newTodos);
+    setTodoText('');
   };
   const handleKeyDown = e => {
     if (e.key === 'Enter') {
       handleAddTodo();
     }
   };
+
+  // 3] deleted
   const handleDeleteTodo = deleteId => {
-    const newTodos = todos.filter(item => item.id !== deleteId);
-    setTodos(newTodos);
-  };
-
-  const handleToggleTodo = (id, done) => {
-    const nextTodos = todos.map(item => {
-      if (item.id === id) {
-        return { ...item, done };
-      }
-      return item;
+    dispatch({
+      type: 'deleted',
+      deleteId,
     });
-    setTodos(nextTodos);
+    // const newTodos = todos.filter(item => item.id !== deleteId);
+    // setTodos(newTodos);
   };
 
+  // 4] 'done'
+  const handleToggleTodo = (id, done) => {
+    dispatch({
+      type: 'done',
+      id,
+      done,
+    });
+    // const nextTodos = todos.map(item => {
+    //   if (item.id === id) {
+    //     return { ...item, done };
+    //   }
+    //   return item;
+    // });
+    // setTodos(nextTodos);
+  };
+
+  // 5] 'reverse'
   const handleReverse = () => {
+    dispatch({
+      type: 'reverse',
+    });
     // const nextTodos = [...todos];
     // nextTodos.reverse();
     // setTodos(nextTodos);
-    setTodos(todos.toReversed());
+    // setTodos(todos.toReversed());
   };
 
   // const handleToggleCopyTodo = (id, done) => {

86d09395fea5972e3b6ee2a6a1e7b4558fba3ca7
sanggusseu <sanggusseu@gmail.com>
2024-09-17 09:04:37 +0900

    배열 업데이트 하기 2탄

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -4,6 +4,7 @@ import TodoList from './components/todo/TodoList';
 
 export default function AppTodo() {
   const [todoText, setTodoText] = useState('');
+  const [todoIndex, setTodoIndex] = useState(0);
   const [todos, setTodos] = useState([
     { id: 0, text: 'HTML&CSS 공부하기', done: false },
     { id: 1, text: '자바스크립트 공부하기', done: false },
@@ -14,7 +15,14 @@ export default function AppTodo() {
   };
 
   const handleAddTodo = () => {
-    setTodos([...todos, { id: todos.length + 2, text: todoText, done: false }]);
+    const newArr = [...todos];
+    newArr.splice(todoIndex, 0, {
+      id: todos.length + 2,
+      text: todoText,
+      done: false,
+    });
+
+    setTodos(newArr);
     setTodoText('');
   };
 
@@ -38,18 +46,41 @@ export default function AppTodo() {
     });
     setTodos(nextTodos);
   };
+
+  const handleChangeTodoIndex = e => {
+    setTodoIndex(e.target.value);
+  };
   return (
     <div>
       <h2>할일목록</h2>
-      <input
-        type='text'
-        onChange={handleTodoTextChange}
-        value={todoText}
-        onKeyDown={handleKeyDown}
-      />
-      <button type='button' onClick={handleAddTodo}>
-        추가
-      </button>
+      <div>
+        <input
+          type='text'
+          onChange={handleTodoTextChange}
+          value={todoText}
+          onKeyDown={handleKeyDown}
+        />
+        <button type='button' onClick={handleAddTodo}>
+          추가
+        </button>
+      </div>
+      <div>
+        <select
+          name=''
+          id=''
+          value={todoIndex}
+          onChange={handleChangeTodoIndex}
+        >
+          {todos.map((_, index) => (
+            <option value={index} key={index}>
+              {index} 번째
+            </option>
+          ))}
+        </select>
+        <button type='button' onClick={handleAddTodo}>
+          {todoIndex}번째 추가
+        </button>
+      </div>
       <div>Preview: {todoText}</div>
       <TodoList
         todos={todos}

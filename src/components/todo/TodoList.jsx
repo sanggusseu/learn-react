b5ad236b3358b651d553fc594eb7266b26c35503
sanggusseu <sanggusseu@gmail.com>
2024-09-18 14:07:58 +0900

    useMemo Hook

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,17 +1,52 @@
 import TodoItem from './TodoItem';
 import { useTodos } from '../../context/TodoContext';
+import { useEffect, useMemo, useState } from 'react';
 
 function TodoList() {
   const todos = useTodos();
+  const [isDone, setIsDone] = useState(false);
+
+  const getFilteredTodos = () => {
+    if (!isDone) {
+      return todos;
+    }
+    return todos.filter(item => item.done === true);
+  };
+
+  const getStatsCount = () => {
+    console.log('getStatsCount 함수 실행!');
+    const totalCount = todos.length;
+    const doneCount = todos.filter(item => item.done === true).length;
+    return { totalCount, doneCount };
+  };
+
+  const { totalCount, doneCount } = useMemo(() => getStatsCount(), [todos]);
+
+  const filteredTodos = getFilteredTodos();
 
   return (
-    <ul>
-      {todos.map(item => (
-        <li key={item.id}>
-          <TodoItem item={item} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <input
+          id='isDone'
+          type='checkbox'
+          checked={isDone}
+          onChange={e => {
+            setIsDone(e.target.checked);
+          }}
+        />
+        <label htmlFor='isDone'>
+          완료된 항목 보기({doneCount}/{totalCount})
+        </label>
+      </div>
+      <ul>
+        {filteredTodos.map(item => (
+          <li key={item.id}>
+            <TodoItem item={item} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
